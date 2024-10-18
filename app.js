dbf6555088e085fc10bfab64ad804ea9687c2db0
kevin corporativo <kmosorio50@misena.edu.co>
2024-10-18 16:45:04 -0500

    Commit 3

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 const calculoRouters = require("./routes/calcularRoute");
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.json());
 
 app.use("/api", calculoRouters);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Runnin port link ${port}`);
 });
